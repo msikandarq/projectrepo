1261ea75a4227cb62c6bcc04aa480b10bd51daa4
Mohammad Sikandar Quershi <sikandarq@newtglobal.chn.local>
2017-07-20 13:46:27 +0530

    project committed

---
+++ src/main/java/com/newt/service/SampleServiceImpl.java
@@ -0,0 +1,41 @@
+package com.newt.service;
+
+import java.util.List;
+
+import com.newt.bean.Item;
+
+public class SampleServiceImpl {
+
+	public void getItems() {
+
+	}
+
+	public double calculateGST(List<Item> items) {
+		double finalTotal = 0;
+		for (Item item : items) {
+			double itemTotal = item.getPrice() * item.getItemQty();
+			finalTotal += itemTotal;
+		}
+
+		// 10% GST
+		double priceAfterGST = finalTotal + (finalTotal / 10);
+		return priceAfterGST;
+	}
+
+	public double calcualteTax(List<Item> items) {
+		double finalTotal = 0;
+		double vatRate = 12.5;
+		double serviceTaxRate = 2.5;
+
+		for (Item item : items) {
+			double itemTotal = item.getPrice() * item.getItemQty();
+			finalTotal += itemTotal;
+		}
+
+		double vatAmt = finalTotal / vatRate;
+		double serviceTaxAmt = finalTotal / serviceTaxRate;
+		double totalAfterTax = finalTotal + vatAmt + serviceTaxAmt;
+
+		return totalAfterTax;
+	}
+}
---
+++ src/test/java/com/newt/test/SampleTest.java
@@ -0,0 +1,61 @@
+package com.newt.test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.newt.bean.Item;
+import com.newt.service.SampleServiceImpl;
+
+public class SampleTest {
+	@Test
+	public void validateGSTPass() {
+		SampleServiceImpl sampleServiceImpl = new SampleServiceImpl();
+
+		List<Item> items = new ArrayList<Item>();
+
+		Item item = new Item();
+		item.setName("Laptop");
+		item.setItemQty(1);
+		item.setPrice(30000);
+
+		items.add(item);
+
+		assertEquals(sampleServiceImpl.calculateGST(items), 3000.0, 0.0);
+	}
+	
+	@Test
+	public void validateGSTFail() {
+		SampleServiceImpl sampleServiceImpl = new SampleServiceImpl();
+
+		List<Item> items = new ArrayList<Item>();
+
+		Item item = new Item();
+		item.setName("Laptop");
+		item.setItemQty(1);
+		item.setPrice(30000);
+
+		items.add(item);
+
+		assertEquals(sampleServiceImpl.calculateGST(items), 300.0, 0.0);
+	}
+	
+	@Test
+	public void validateTaxPricePass() {
+		SampleServiceImpl sampleServiceImpl = new SampleServiceImpl();
+		List<Item> items = new ArrayList<Item>();
+
+		Item item = new Item();
+		item.setName("Laptop");
+		item.setItemQty(1);
+		item.setPrice(30000);
+
+		items.add(item);
+
+		assertEquals(sampleServiceImpl.calcualteTax(items), 44400.0, 0.0);
+	}
+
+}
